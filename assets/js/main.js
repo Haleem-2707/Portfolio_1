d2cd02b03d5a4621ff3756389ef1a6dd421f812c
unknown <Haleem-2707>
2023-11-22 20:12:53 -0800

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,8 +82,73 @@ const sendEmail = (e) => {
 contactForm.addEventListener("submit", sendEmail);
 /*=============== SHOW SCROLL UP ===============*/
 
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+
+  //when the scroll is higher than 350 viewport height
+  this.scrollY >= 350
+    ? scrollUp.classList.add("scroll-up")
+    : scrollUp.classList.remove("scroll-up");
+};
+
+window.addEventListener("scroll", scrollUp);
+
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id");
+    sectionsClass = document.querySelector(
+      ".nav__menu a[href*=' + sectionId + ']"
+    );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 /*=============== DARK LIGHT THEME ===============*/
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "ri-sun-line";
 
+//previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+//we obtain the current theme that the interface has by validating the dark theme class
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+
+//We validate if the user previously chose a topic
+if (selectedTheme) {
+  //If the validation is fullfilled, we ask what the issue was to know if we activated or
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](
+    iconTheme
+  );
+}
+
+//Activate / deactivate the theme manually with the button
+themeButton.addEventListener("click", () => {
+  //Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  //we save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
